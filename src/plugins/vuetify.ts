67e965c01532a0dff3a558fb2b49832d82988ed6
SQMoonGit <hykmoon@gmail.com>
2021-07-10 23:15:08 -0500

    First commit

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: "fa",
+  },
+  theme: {
+    dark: true,
+  },
+});
